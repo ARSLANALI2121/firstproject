c2e12ecb9f0da5c6065b1968ef708a308bcf5759
ARSLANALI2121 <ARSLAN.ORAXTECH@gmail.com>
2021-03-02 19:11:31 +0500

    Navbar and Favicon To Chat Application

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+import Turbolinks from 'turbolinks';
 import * as ActiveStorage from "@rails/activestorage"
+import 'semantic-ui-sass';
 import "channels"
 
+
 Rails.start()
+require("semantic-ui-sass");
 Turbolinks.start()
+
 ActiveStorage.start()
+
+$(document).on('turbolinks:load',function() {
+	
+$('.ui.dropdown').dropdown();
+});
